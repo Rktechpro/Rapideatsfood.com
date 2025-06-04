dd79f4a30726f9a0bd144f98809b12012336c9c0
Rk <ravikumar90949@gmail.com>
2025-06-04 15:17:16 +0530

    Change your menu

--- Frontend/src/page/Home/Home.jsx
+++ Frontend/src/page/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../../components/Header/Header";
-// import Menu from "../../components/menu/Menu";
+import Menu from "../../components/menu/Menu";
 import Food_disp from "../../components/food_item_display/Food_disp";
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="Home" id="Home">
       <Header />
       <div>
-        {/* <Menu category={category} setCatgory={setCatgory} /> */}
+        <Menu category={category} setCatgory={setCatgory} />
       </div>
       <Food_disp category={category} />
     </div>
